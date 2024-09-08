9d517920b365d0becbf6db1305d72c5408420d0d
YangYunjia <yyj980401@126.com>
2024-09-08 14:27:04 +0800

    optimize inference pipeline

--- examples/wing/app.py
+++ examples/wing/app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request, jsonify, send_file, current_app
-import numpy as np
 import os
 import subprocess
 import time
@@ -7,39 +6,63 @@ import json
 
 app = Flask(__name__)
 
+# establish the wing api instance at the beginning of the client
+# later use it to predict wing results given input parameters
+from flowvae.app.wing.wing_api import Wing_api
+wing_api = Wing_api(device='cuda:0')
+
+def process_test_input(inputs):
+    '''
+    predict wing results given the input
+
+    paras:
+    ===
+    `inputs` (`list`) the input parameters (every element should be float)
+        > ONLY containes the required values! (len = 8 + 21)
+
+    returns:
+    ===
+    a `list` of CL, CD, CM
+    '''
+    wg = wing_api.predict(inputs)
+    wg.plot_2d(['upper', 'full'], contour=9,  write_to_file=os.path.join(current_app.root_path, 'image', 'wg.png'))
+    wg.lift_distribution()
+    cl_array = wg.cl
+    # print(f"Processing test input: {test_input}")
+    # print(f"CL: {cl_array}")
+    return list(cl_array)
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# 处理AJAX请求并调用run.py中的函数
+# handle AJAX requests and conduct inference
 @app.route('/process_input', methods=['POST'])
 def process_input():
+
+    t1 = time.time()    # start time
+
     input_num = request.json['test_input']
-    # 获取test_input
-    test_input = np.take(input_num, list(range(0, 8)) + list(range(9, 30)))
-    # change each value to float
-    test_input = [float(i) for i in test_input]
+    # fetch inputs
+    inputs = [float(input_num[i]) for i in list(range(0, 8)) + list(range(9, 30))]
 
-    result = subprocess.run(
-            ["python3", "run.py", json.dumps(test_input)],
-            capture_output=True,
-            text=True
-        )
-
-        # 获取子进程输出（cl_array）
-    cl_array = result.stdout.split('\n')[-2]
+    # inference results
+    cl_array = process_test_input(inputs)
 
     # 获取图片的修改时间
-    image_path = os.path.join(current_app.root_path, 'image/wg.png')
+    image_path = os.path.join(current_app.root_path, 'image', 'wg.png')
     mod_time = os.path.getmtime(image_path)
     formatted_time = time.strftime('%Y%m%d%H%M%S', time.localtime(mod_time))  # 使用时间戳
     image_url = f'/static/image/wg.png?t={formatted_time}'  # 在URL后附加时间戳}
 
+    t2 = time.time()
+
     return jsonify({
-        'status': 'success',
-        'data': test_input,
+        'status': 'success', 
+        'data': inputs,
         'cl_array': cl_array,
-        'image_url': image_url
+        'image_url': image_url,
+        'inference_time': t2 - t1
     })
     
     
@@ -49,4 +72,4 @@ def get_image():
     return send_file('image/wg.png', mimetype='image/png')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
